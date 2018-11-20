f41ea9bc96c481fb3b5f79af566157c0d01e808c
tonyli <yuguang.li@hotmail.com>
2018-11-20 14:05:28 +0800

    updateqqqq

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello Tony"}
+		res := &response{Message: "Hello Tony hello"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
